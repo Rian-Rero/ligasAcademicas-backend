371ce3b32b349c641edc5f1592465ec54c7c9615
Rian-Rero <rian@dharma.com.br>
2026-04-29 15:40:30 -0300

    fix: validation errors

--- src/validators/validate.js
+++ src/validators/validate.js
@@ -1,15 +1,13 @@
 export default function validate(validator) {
   return (request) =>
     validator
-      .transform(
-        ({ params, query, body, file, files, signedCookies = {} }) => ({
-          ...(!!params && params),
-          ...(!!query && query),
-          ...(!!body && body),
-          ...(!!file && file), // Multer .single
-          ...(!!files && files), // Multer .fields
-          ...(!!signedCookies && signedCookies),
-        }),
-      )
+      .transform(({ params, query, body, file, files, signedCookies }) => ({
+        ...(!!params && params),
+        ...(!!query && query),
+        ...(!!body && body),
+        ...(!!file && file), // Multer .single
+        ...(!!files && files), // Multer .fields
+        ...(!!signedCookies && signedCookies),
+      }))
       .parse(request);
 }
